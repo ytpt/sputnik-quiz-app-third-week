276504903a5f8b64c4780b6a0b7c3f5f206fcfbb
Annarchive <ytpt@mail.ru>
2023-07-22 22:52:10 +0400

    Refactored path

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const router = require("./router/index");
+const router = require("./router");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const PORT = process.env.PORT || 5000;
