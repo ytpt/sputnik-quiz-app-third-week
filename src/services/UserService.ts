9def6c88f28a51499dbf605b1b2a051948ef8e80
Annarchive <ytpt@mail.ru>
2023-07-26 15:35:37 +0400

    Deleted file UserService.ts

--- src/services/UserService.ts
+++
@@ -1,9 +0,0 @@
-import $api from "../http/index";
-import { AxiosResponse } from "axios";
-import { IUser } from "../models/response/IUser";
-
-export default class UserService {
-    static fetchUsers(): Promise<AxiosResponse<IUser[]>> {
-        return $api.get<IUser[]>("/users");
-    }
-}
