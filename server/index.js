b56525dbc4864bc14878d54084d29117f00691fb
Annarchive <ytpt@mail.ru>
2023-07-27 22:36:47 +0400

    Deleted compression

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const compression = require("compression");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
@@ -10,30 +9,24 @@ const errorMiddleware = require("./middlewares/error-middleware");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(compression());
-app.use(compression({filter: shouldCompress}));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'https://sputnik-quiz-app-third-week.vercel.app',
+    origin: 'http://localhost:3001',
 }));
 app.use("/api", router);
 app.use(errorMiddleware);
 
-
-function shouldCompress() {
-    const start = async () => {
-        try {
-            await mongoose.connect(process.env.DB_URL, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
-        } catch (e) {
-            console.log(e);
-        }
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    } catch (e) {
+        console.log(e);
     }
-    start();
 }
+start();
